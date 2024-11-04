19b5103075f1ab74773cc82634e5ec024cfc8920
tomislavjezidzic <tjezidzic@outlook.com>
2024-11-04 16:19:00 +0100

    init

--- components/organisms/ThreeDMap/ThreeDMap.tsx
+++ components/organisms/ThreeDMap/ThreeDMap.tsx
@@ -1,56 +1,71 @@
 import styles from './ThreeDMap.module.scss';
 import dynamic from 'next/dynamic';
-import { useEffect, useRef, useState } from 'react';
-const Globe = dynamic(import('react-globe.gl'), { ssr: false });
-
+import React, { useCallback, forwardRef, useEffect, useState, createRef } from 'react';
 import * as d3 from 'd3';
+import gsap from 'gsap';
+import { GlobeMethods } from 'react-globe.gl';
+import GEO_DATA from './data/countries_hex_data.json';
 
-export interface ThreeDMapProps {}
+const GlobeComponent = dynamic(() => import('../../globe'), {
+    ssr: false,
+});
+
+const Globe = React.memo(
+    forwardRef((props: any, ref) => <GlobeComponent {...props} forwardRef={ref} />)
+);
+
+interface ThreeDMapProps {}
 
 const ThreeDMap = ({}: ThreeDMapProps) => {
-    const globeEl = useRef(null);
-    const [popData, setPopData] = useState([]);
+    const globeRef = createRef();
+    const [countries, setCountries] = useState([]);
+    const [globeSpeed, setGlobeSpeed] = useState(0);
+    const [populationData, setPopulationData] = useState([]);
+
+    useEffect(() => setCountries(GEO_DATA.features), []);
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
-            // load data
             fetch('/world_population.csv')
                 .then(res => res.text())
-                .then(csv =>
-                    d3.csvParse(csv, ({ lat, lng, pop }) => ({ lat: +lat, lng: +lng, pop: +pop }))
-                )
-                .then(setPopData);
+                .then(csv => d3.csvParse(csv, ({ lat, lng, pop }) => ({ lat: +lat, lng: +lng })))
+                .then(data => setPopulationData(data.flat()));
+        }
+    }, []);
+
+    const handleRef = useCallback(() => {
+        if (globeRef.current) {
+            globeRef.current.controls().autoRotate = true;
+            setGlobeSpeed(0.2);
+            globeRef.current.controls().enableZoom = false;
         }
     }, []);
 
     useEffect(() => {
-        // Auto-rotate
-        if (typeof window !== 'undefined') {
-            // globeEl.current.controls().autoRotate = true;
-            // globeEl.current.controls().autoRotateSpeed = 0.1;
+        if (globeRef.current) {
+            globeRef.current.controls().autoRotate = true;
+            globeRef.current.controls().autoRotateSpeed = globeSpeed;
+            globeRef.current.controls().enableZoom = false;
         }
-    }, []);
-
-    const weightColor = d3.scaleSequentialSqrt(d3.interpolateYlOrRd).domain([0, 1e7]);
+    }, [globeRef.current, globeSpeed]);
 
     return (
-        <section className="">
-            {typeof window !== 'undefined' && (
-                <Globe
-                    ref={globeEl}
-                    globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                    bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                    backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-                    hexBinPointsData={popData}
-                    hexBinPointWeight="pop"
-                    hexAltitude={d => d.sumWeight * 6e-8}
-                    hexBinResolution={4}
-                    hexTopColor={d => weightColor(d.sumWeight)}
-                    hexSideColor={d => weightColor(d.sumWeight)}
-                    hexBinMerge={true}
-                    enablePointerInteraction={false}
-                />
-            )}
+        <section className={styles.threeDMap}>
+            <Globe
+                ref={globeRef}
+                polygonsData={countries}
+                polygonAltitude={0.005}
+                polygonCapColor={() => 'transparent'}
+                polygonSideColor={() => 'transparent'}
+                polygonStrokeColor={() => 'red'}
+                pointsData={populationData}
+                pointLat={d => d.lat}
+                pointLng={d => d.lng}
+                pointAltitude={0}
+                pointRadius={0.1}
+                pointColor={() => 'white'}
+                onGlobeReady={handleRef}
+            />
         </section>
     );
 };
