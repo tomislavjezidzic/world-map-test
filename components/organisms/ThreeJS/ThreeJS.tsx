29b65ca51bc8782e59a40f1b367fdc50c38f5e92
tomislavjezidzic <tjezidzic@outlook.com>
2024-11-28 12:04:37 +0100

    tweaking

--- components/organisms/ThreeJS/ThreeJS.tsx
+++ components/organisms/ThreeJS/ThreeJS.tsx
@@ -32,6 +32,17 @@ interface ThreeJSProps {
     }[];
 }
 
+// if (
+//     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+//         navigator.userAgent,
+//     )
+//     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+//         navigator.userAgent.substr(0, 4),
+//     )
+// ) {
+//     isMobile = true;
+// }
+
 const ThreeJS = ({ continentsData, isFlat = false }: ThreeJSProps) => {
     const $globeRef = useRef<HTMLDivElement>(null);
     const $w = useRef(null);
@@ -51,6 +62,8 @@ const ThreeJS = ({ continentsData, isFlat = false }: ThreeJSProps) => {
     const $labels = useRef([]);
     const [activePoint, setActivePoint] = useState(null);
     const [labelsLoaded, setLabelsLoaded] = useState(false);
+    const [touchPosition, setTouchPosition] = useState(null);
+    const [prevTouchPosition, setPrevTouchPosition] = useState(null);
     const $threeGeoJSON = useRef(null);
 
     // useEffect(() => {
@@ -391,38 +404,41 @@ const ThreeJS = ({ continentsData, isFlat = false }: ThreeJSProps) => {
         };
     };
 
-    const handleGlobeRotationEnd = useCallback(() => {
-        const beta = $controls.current.getPolarAngle() - Math.PI / 2;
+    const handleGlobeRotationEnd = useCallback(
+        (event: any) => {
+            if (activePoint !== null) return;
+            const beta = $controls.current.getPolarAngle() - Math.PI / 2;
 
-        gsap.fromTo(
-            $controls.current,
-            {
-                minPolarAngle: Math.PI / 2 + beta,
-                maxPolarAngle: Math.PI / 2 + beta,
-            },
-            {
-                minPolarAngle: Math.PI / 2,
-                maxPolarAngle: Math.PI / 2,
-                duration: 1,
-                ease: 'power3.out',
-                onComplete: () => {
-                    $controls.current.minPolarAngle = 0;
-                    $controls.current.maxPolarAngle = Math.PI;
+            gsap.fromTo(
+                $controls.current,
+                {
+                    minPolarAngle: Math.PI / 2 + beta,
+                    maxPolarAngle: Math.PI / 2 + beta,
                 },
-            }
-        );
-    }, []);
-
-    const debouncedHandleGlobeRotationEnd = debounce(handleGlobeRotationEnd, 3000);
+                {
+                    minPolarAngle: Math.PI / 2,
+                    maxPolarAngle: Math.PI / 2,
+                    duration: 1,
+                    ease: 'power3.out',
+                    onComplete: () => {
+                        $controls.current.minPolarAngle = 1;
+                        $controls.current.maxPolarAngle = Math.PI * 0.7;
+                    },
+                }
+            );
+        },
+        [activePoint]
+    );
 
     useEffect(() => {
+        const debouncedHandleGlobeRotationEnd = debounce(handleGlobeRotationEnd, 3000);
         if ($controls.current) {
             $controls.current.addEventListener('end', debouncedHandleGlobeRotationEnd, false);
         }
         return () => {
             $controls?.current?.removeEventListener('end', debouncedHandleGlobeRotationEnd, false);
         };
-    }, [$controls.current]);
+    }, [$controls.current, handleGlobeRotationEnd, activePoint]);
 
     const Globe = useCallback(() => {
         $w.current = $globeRef.current.offsetWidth;
@@ -495,6 +511,8 @@ const ThreeJS = ({ continentsData, isFlat = false }: ThreeJSProps) => {
             $controls.current.autoRotateSpeed = 0.3;
             $controls.current.enableZoom = false;
             $controls.current.enablePan = false;
+            $controls.current.minPolarAngle = Math.PI / 2;
+            $controls.current.maxPolarAngle = Math.PI / 2;
             $controls.current.dampingFactor = 0.05;
             $controls.current.screenSpacePanning = false;
         }
@@ -530,26 +548,48 @@ const ThreeJS = ({ continentsData, isFlat = false }: ThreeJSProps) => {
         animate();
     }, []);
 
+    useEffect(() => {
+        if (prevTouchPosition !== null && touchPosition !== null) {
+            let offset = prevTouchPosition - touchPosition;
+
+            window.scrollTo(0, window.scrollY + offset);
+        }
+
+        setPrevTouchPosition(touchPosition);
+    }, [touchPosition, prevTouchPosition]);
+
     return (
-        <div className={styles.main}>
+        <div
+            className={styles.main}
+            onTouchMoveCapture={useCallback(ev => {
+                setTouchPosition(ev.changedTouches[0].screenY);
+            }, [])}
+            onTouchEnd={() => {
+                setTouchPosition(null);
+                setPrevTouchPosition(null);
+            }}
+        >
             <div
                 className={styles.canvas}
                 ref={$globeRef}
                 onClick={() => handleClick([0, 0], null)}
+                onTouchEnd={() => handleClick([0, 0], null)}
             ></div>
 
             <div className={styles.markers}>
                 {continentData.features.map((feature, i) => {
                     if (feature.pointCoordinates) {
                         return (
-                            <div
+                            <button
                                 className={cn(styles.marker, {
                                     [styles.isActive]: activePoint === i,
                                     [styles.isLoaded]: labelsLoaded,
                                 })}
-                                key={i}
+                                key={`continent-marker-${i}`}
                                 id={`${feature.id}-marker`}
                                 onClick={() => handleClick(feature.pointCoordinates, i)}
+                                type="button"
+                                onTouchEnd={() => handleClick(feature.pointCoordinates, i)}
                             >
                                 <ThreeJSMapDataTooltip
                                     canvasDimensions={{
@@ -562,7 +602,7 @@ const ThreeJS = ({ continentsData, isFlat = false }: ThreeJSProps) => {
                                     isActive={activePoint === i}
                                     data={continentsData.find(data => data.id === feature.id)}
                                 />
-                            </div>
+                            </button>
                         );
                     }
                 })}
